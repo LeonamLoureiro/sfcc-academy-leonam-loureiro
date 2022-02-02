b2e12d35993ba79f015bc5633aa69d89d3784c4e
LLoureiro1 <leonamloureiro1@gmail.com>
2022-02-02 18:10:14 -0300

    Webpack Adjustments
    I solved the webpack issue and some more adjustments in the javascript code that allowed to show/hide

---
+++ cartridges/app_leonam_exercises/cartridge/client/default/js/custom/hide-show.js
@@ -0,0 +1,18 @@
+
+var hide = document.querySelector('#labelpromocode');
+var show = document.querySelector('#fieldpromocode');
+hide.addEventListener('click', function() {
+    
+    hide.style.display = 'none';
+    show.style.display = 'block';
+
+ 
+});
+
+
+//using jquery
+//$('#fieldpromocode').hide();
+//$('#labelpromocode').on('click', function(){
+  //$('#labelpromocode').hide();
+  //$('#fieldpromocode').show();
+//})
