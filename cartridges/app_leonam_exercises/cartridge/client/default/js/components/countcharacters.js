31370eb6f855a750a65ba86f2a02fcada2aa5e94
LLoureiro1 <leonamloureiro1@gmail.com>
2022-02-07 18:19:01 -0300

    update javascript
    Changes to countcharacters.js file for better display of character counter

--- cartridges/app_leonam_exercises/cartridge/client/default/js/components/countcharacters.js
+++ cartridges/app_leonam_exercises/cartridge/client/default/js/components/countcharacters.js
@@ -1,15 +1,15 @@
 //'use strict';
+    var limit = 150;
+
+    $("#countcharacters").text("You have "+ limit + " characters left out of "+ limit);
 
 $("#comments").on("keyup", function () {
-    var limit = 150;
+    
     var charactersTyped = $(this).val().length;
     var charactersRemaining = limit - charactersTyped;
 
-    if(charactersTyped == 0 || charactersTyped == limit)
-    {
-        
-        $("#countcharacters").text("You have "+ charactersRemaining + " characters left out of "+ limit);
-    } else{
-        $("#countcharacters").text(charactersRemaining + " characters left ");
-    }
+    $("#countcharacters").text("You have "+ charactersRemaining + " characters left out of "+ limit);
+    
+       
+    
 });
