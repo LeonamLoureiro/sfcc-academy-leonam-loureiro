4a3a58e494e4f1d610c232b7acd3328029d88de1
LLoureiro1 <leonamloureiro1@gmail.com>
2022-02-09 23:39:16 -0300

    Create Structure
    Import slick, adjust css, create js file and display content slot in template

---
+++ cartridges/app_leonam_exercises/cartridge/client/default/js/components/carousel.js
@@ -0,0 +1,25 @@
+'use strict';
+
+$(document).ready(function(){
+    require('../../js/components/slick/slick/slick');
+    $('.carousel').slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      responsive: [
+        
+        
+        {
+          breakpoint: 540,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        
+      ]
+    });    
+    
+  });
